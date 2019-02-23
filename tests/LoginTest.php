de20b1b09f13c609041cf3d9e4978f2b62af50b2
Aam Surganda <surganda@gmail.com>
2019-02-23 13:14:43 +0700

    add: fail login test

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -36,4 +36,28 @@ class LoginTest extends TestCase
         $response = $this->call('PATCH', '/api/v1/checklists', [], [], [], $server);
         $this->assertEquals(405, $response->status());
     }
+    
+    /**
+     * failed login will return 401
+     *
+     * @return void
+     */
+    public function testFailLogin()
+    {
+        $response = $this->call('GET', '/api/v1/login?email=surganda@gmail.com&password=12345');
+        
+        $this->assertEquals(401, $response->status());
+    }
+    
+    /**
+     * wrong path test
+     *
+     * @return void
+     */
+    public function testNotFoundLogin()
+    {
+        $response = $this->call('GET', '/api/login?email=surganda@gmail.com&password=123456');
+        
+        $this->assertEquals(404, $response->status());
+    }
 }
